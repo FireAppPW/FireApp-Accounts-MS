571c6d3cd730d9daea7c399ca6b9430ffdd81c16
Truong Giang Do <giangh14cqt@gmail.com>
2023-06-09 14:45:18 +0200

    Update ci

--- src/main/java/pw/ersms/accounts/AccountsApplication.java
+++ src/main/java/pw/ersms/accounts/AccountsApplication.java
@@ -4,17 +4,16 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @OpenAPIDefinition(
-		servers = {
-				@Server(url = "/", description = "Default Server URL")
-		}
+        servers = {
+                @Server(url = "/", description = "Default Server URL")
+        }
 )
 @SpringBootApplication
 public class AccountsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsApplication.class, args);
+    }
 }
