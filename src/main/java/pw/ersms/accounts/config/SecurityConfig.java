e231371d92d4f7d64890a6fc6bef09d1cf29c13a
Truong Giang Do <giangh14cqt@gmail.com>
2023-06-11 19:03:13 +0200

    Fix cors

--- src/main/java/pw/ersms/accounts/config/SecurityConfig.java
+++ src/main/java/pw/ersms/accounts/config/SecurityConfig.java
@@ -1,21 +1,15 @@
 package pw.ersms.accounts.config;
 
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 
 @Configuration
@@ -42,13 +36,26 @@ public class SecurityConfig {
                     return corsCfg;
                 })
                 .and()
-                .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/account/login/**").permitAll()
-                        .requestMatchers("/account/**").hasAnyAuthority("SysAdmin", "FireAdmin", "User")
-                        .anyRequest().permitAll()
-                )
+                .authorizeHttpRequests()
+//                .requestMatchers("/account/register",
+//                        "/account/verify**",
+//                        "/account/login",
+//                        "/swagger-ui/**",
+//                        "/v3/api-docs/**")
+                .requestMatchers("/*")
+                .permitAll()
+//                .anyRequest()
+//                .authenticated()
+                .and()
+//                .authorizeHttpRequests((authz) -> authz
+//                        .requestMatchers("/account/login/**").permitAll()
+//                        .requestMatchers("/account/**").hasAnyAuthority("SysAdmin", "FireAdmin", "User")
+//                        .anyRequest().permitAll()
+//                )
                 .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
 
         http.exceptionHandling()
                 .authenticationEntryPoint(
@@ -60,25 +67,8 @@ public class SecurityConfig {
                         }
                 );
 
-        http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+//        http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
-
-    // To enable CORS
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        final CorsConfiguration configuration = new CorsConfiguration();
-//
-////        configuration.setAllowedOrigins(ImmutableList.of("https://www.yourdomain.com")); // www - obligatory
-//        configuration.setAllowedOrigins(List.of("*"));  //set access from all domains
-//        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-//        configuration.setAllowCredentials(true);
-//        configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
-//
-//        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//
-//        return source;
-//    }
 }
