af171bbd2c7dec71359aa5b94eeb1bab6af2da18
Truong Giang Do <giangh14cqt@gmail.com>
2023-06-11 18:24:10 +0200

    Fix cors

--- src/main/java/pw/ersms/accounts/config/WebConfig.java
+++ src/main/java/pw/ersms/accounts/config/WebConfig.java
@@ -5,12 +5,21 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//@Configuration
+//@EnableWebMvc
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**");
+//    }
+//}
+
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedMethods("*");
     }
 }
