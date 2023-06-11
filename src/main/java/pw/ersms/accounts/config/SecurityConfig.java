b2e3e41afe47454c9926cee9c7b98f738dca0f47
Truong Giang Do <73480323+giangh14cqt@users.noreply.github.com>
2023-06-11 18:09:58 +0200

    Add cors (#15)

--- src/main/java/pw/ersms/accounts/config/SecurityConfig.java
+++ src/main/java/pw/ersms/accounts/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package pw.ersms.accounts.config;
 
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -23,13 +26,10 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         //allow all requests (even if the user doesnt have credentials)
+
         http
-                .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/account/login/**").permitAll()
-                        .requestMatchers("/account/**").hasAnyAuthority("SysAdmin", "FireAdmin", "User")
-                        .anyRequest().permitAll()
-                )
-                .csrf().disable()
+                .csrf()
+                .disable()
                 .cors().configurationSource(c -> {
                     CorsConfiguration corsCfg = new CorsConfiguration();
                     corsCfg.applyPermitDefaultValues();
@@ -37,6 +37,14 @@ public class SecurityConfig {
                     corsCfg.addAllowedMethod(CorsConfiguration.ALL);
                     return corsCfg;
                 });
+
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/account/login/**").permitAll()
+                        .requestMatchers("/account/**").hasAnyAuthority("SysAdmin", "FireAdmin", "User")
+                        .anyRequest().permitAll()
+                )
+                .csrf().disable();
         http.exceptionHandling()
                 .authenticationEntryPoint(
                         (request, response, ex) -> {
