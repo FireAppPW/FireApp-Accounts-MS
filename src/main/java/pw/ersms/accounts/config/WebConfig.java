1dbde196fbfe4d2c3494c651f214c2e6af0e1a75
Truong Giang Do <giangh14cqt@gmail.com>
2023-06-11 18:49:23 +0200

    Fix cors

--- src/main/java/pw/ersms/accounts/config/WebConfig.java
+++ src/main/java/pw/ersms/accounts/config/WebConfig.java
@@ -2,19 +2,20 @@ package pw.ersms.accounts.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
-@Configuration(proxyBeanMethods = false)
+@Configuration
 public class WebConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(final CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("*").allowedHeaders("*");
-            }
-        };
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors(httpSecurityCorsConfigurer ->
+                httpSecurityCorsConfigurer.configurationSource(request ->
+                        new CorsConfiguration().applyPermitDefaultValues()
+                )
+        );
+        return http.build();
     }
 }
