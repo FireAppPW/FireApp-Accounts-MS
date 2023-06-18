2c6ae7a1eae2a4cb7471abecb3c0286e90869445
AnejVovcak <anej.vovcak@gmail.com>
2023-06-18 12:47:09 +0200

    fix test

--- src/test/java/pw/ersms/accounts/account/AccountServiceTest.java
+++ src/test/java/pw/ersms/accounts/account/AccountServiceTest.java
@@ -14,7 +14,6 @@ import java.util.Collections;
 import java.util.Optional;
 import java.util.Set;
 
-import static org.apache.hc.core5.http2.ssl.ConscryptSupport.verify;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
